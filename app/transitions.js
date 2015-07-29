20d55f9f556f2be0cc5da4a857d3555b4bd31754
Ian Storz <i@ianstorz.com>
2015-07-29 15:29:20 -0600

    Full example with no transitions

--- app/transitions.js
+++ app/transitions.js
@@ -1,29 +1,3 @@
-let duration = 300;
-
 export default function() {
-  this.setDefault({ duration });
 
-  this.transition(
-    this.fromRoute('index'),
-    this.toRoute('people'),
-    this.use('to-left'),
-    this.reverse('to-right')
-  );
-
-  this.transition(
-    this.fromRoute('people.index'),
-    this.toRoute(function(routeName) { return routeName !== 'index' && routeName !== 'people.index'; }),
-    this.useAndReverse('explode', {
-      matchBy: 'src',
-      use: 'fly-to'
-    },{
-      pickNew: '.person-info',
-      use: 'to-left'
-    },{
-      pickOld: '.person-info',
-      use: 'to-right'
-    },{
-      use: 'cross-fade'
-    })
-  );
 }
